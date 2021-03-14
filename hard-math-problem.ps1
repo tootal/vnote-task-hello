fdabda1d0c008bd9efbdad9152d5385b6376913c
tootal <tootal@yeah.net>
2021-03-14 17:38:54 +0800

    update hard

--- hard-math-problem.ps1
+++
@@ -1,7 +0,0 @@
-Write-Output "::show-question title=你好::你是傻逼吗？"
-$input = Read-Host
-if ($input -eq "16384") {
-    Write-Output "好的，知道了。"
-} else {
-    Write-Output "人要有点自知之明。"
-}
---
+++ hard.ps1
@@ -0,0 +1,6 @@
+Write-Output "::show-message id=msg_intro::"
+$input = Read-Host
+$cnt = 0
+if ($input -eq "0") {
+    
+}
